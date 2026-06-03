b7be74ac21c322eccf80226b042acf94a276159f
asuka0120 <ja7201533@outlook.jp>
2026-06-03 15:13:29 +0900

    api/index.php修正

--- laravel_app/api/index.php
+++ laravel_app/api/index.php
@@ -1,1 +1,8 @@
-<?php require __DIR__ . '/../public/index.php'; 
+<?php
+
+// Vercelのサーバーレス環境用エントリーポイント
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
+require __DIR__ . '/../public/index.php';
