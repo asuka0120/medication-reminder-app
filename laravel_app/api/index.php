c2cdf331824752a7ac257509262b222c09609121
asuka0120 <ja7201533@outlook.jp>
2026-06-05 06:21:06 +0900

    fix: remove vendor from vercelignore, clean index.php

--- laravel_app/api/index.php
+++ laravel_app/api/index.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-echo "PHP Version: " . PHP_VERSION . "\n";
-echo "Document Root: " . __DIR__ . "\n";
 
 define('LARAVEL_START', microtime(true));
 require __DIR__ . '/../vendor/autoload.php';
